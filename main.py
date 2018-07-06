46e47f5ab698c9cd36578d45bae01b755d848a4d
sleekvick <nvonweb@outlook.com>
2018-07-06 20:13:04 +0100

    Make mass predictions

--- main.py
+++ main.py
@@ -4,45 +4,32 @@ import pickle
 import pandas as pd
 import math
 
-
 df = pd.read_csv('./data.csv')
 model = pickle.load(open('./model.pkl', 'rb'))
 app = Flask(__name__)
 CORS(app)
       
-@app.route('/predict')
+@app.route('/predict', methods=['POST'])
 def predict():
-    lng = request.args['lng']
-    lat = request.args['lat']
-    no_bed = request.args['no_bed']
-    no_bath = request.args['no_bath']
-    no_toilets = request.args['no_toilets']
+    locations = request.json['locations']
+    specs = request.json['specs']
 
-    loc = pd.DataFrame({
-        'lng': [lng],
-        'lat': [lat],
-        'no_bed': [no_bed],
-        'no_bath': [no_bath],
-        'no_toilets': [no_toilets]
-    })
+    data = []
+
+    for location in locations:
+        data.append({
+            'lat': location['lat'],
+            'lng': location['lng'],
+            'no_bed': specs['no_bed'],
+            'no_bath': specs['no_bath'],
+            'no_toilets': specs['no_toilets']
+        })
+
+    loc = pd.DataFrame(data)
 
     prediction = model.predict(loc)
-    cost = prediction[0]
 
-    return jsonify(price=round(cost))
-
-@app.route('/range')
-def range():
-    lng = request.args['lng']
-    lat = request.args['lat']
-    no_bed = request.args['no_bed']
-    no_bath = request.args['no_bath']
-    no_toilets = request.args['no_toilets']
-
-    high = df[(df['lng'] == float(lng)) | (df['lat'] <= float(lat)) | (df['no_bath'] <= int(no_bath)) | (df['no_bed'] <= int(no_bed)) | (df['no_toilets'] <= int(no_toilets))]['price'].quantile(0.75)
-    low = df[(df['lng'] == float(lng)) | (df['lat'] <= float(lat)) | (df['no_bath'] <= int(no_bath)) | (df['no_bed'] <= int(no_bed)) | (df['no_toilets'] <= int(no_toilets))]['price'].quantile(0.25)
-    
-    return jsonify(low=low, high=high)
+    return jsonify(prices=prediction.tolist())
 
 if __name__ == '__main__':
 	app.run()
