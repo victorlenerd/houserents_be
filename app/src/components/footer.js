fd2d71fed11d23b33a5fafcff6d555f975407bbb
sleekvick <nvonweb@outlook.com>
2018-07-10 01:36:06 +0100

    Add github star button

--- app/src/components/footer.js
+++ app/src/components/footer.js
@@ -9,12 +9,8 @@ export default () => (
     <div className="footer">
         <div className="container">
             <div className="footer-left col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                <p>Share with your friends on</p>
-                <div className="share-icons">
-                <img src={whatsapp} />
-                <img src={facebook} />
-                <img src={twitter} />
-                </div>
+                <p>Star on GitHub</p>
+                <a class="github-button" href="https://github.com/victorlenerd/houserents" data-size="large" data-show-count="true" aria-label="Star victorlenerd/houserents on GitHub">Star</a>
             </div>
             <div className="footer-right col-lg-6 col-md-6 col-sm-6 col-xs-12">
                 <div className="signature pull-right">
