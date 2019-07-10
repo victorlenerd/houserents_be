3ea92fa47f1ad8d626ed26a323db6487379681a5
sleekvick <nvonweb@outlook.com>
2019-07-10 18:03:37 -0500

    Log port running

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ DEBUG = os.environ["ENV"] == "DEV"
 
 DBConnector(host=HOST, db_name=DB_NAME, db_user=DB_USER, db_password=DB_PASSWORD, db_port=DB_PORT)
 
-print("API Server Started")
+print('API Server Started On Port ', DB_PORT)
 
 app = Flask(__name__)
 CORS(app)
