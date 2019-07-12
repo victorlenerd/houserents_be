28c53f9ce254155ba8884fc12553fef71af15613
sleekvick <nvonweb@outlook.com>
2019-07-12 04:40:46 -0500

    Use correct name for ecr repo

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 $(aws ecr get-login --region us-east-2 --no-include-email)
-docker pull 699011322781.dkr.ecr.us-east-2.amazonaws.com/houserents-api-develop:latest
+docker pull 699011322781.dkr.ecr.us-east-2.amazonaws.com/houserents-be-develop:latest
 docker run -d -p 5000:5000  \
     --env-file /home/ubuntu/.houserents-api-env \
     --name houserents-api \
-    --rm 699011322781.dkr.ecr.us-east-2.amazonaws.com/houserents-api-develop:latest
+    --rm 699011322781.dkr.ecr.us-east-2.amazonaws.com/houserents-be-develop:latest
