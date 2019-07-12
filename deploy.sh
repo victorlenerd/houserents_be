328baec23f7a18f61812da83703164ba5c319c2b
sleekvick <nvonweb@outlook.com>
2019-07-12 05:08:42 -0500

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 $(aws ecr get-login --region us-east-2 --no-include-email)
 docker pull 699011322781.dkr.ecr.us-east-2.amazonaws.com/houserents-be-develop:latest
+docker container rm houserents-api
 docker run -d -p 5000:5000  \
     --env-file /home/ubuntu/.houserents-api-env \
     --name houserents-api \
